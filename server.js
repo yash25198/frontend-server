f76ec18f3d1d43b152018958841c095fdbd62e29
Yashwanth Reddy <yashwanth@renlans.org>
2022-08-01 18:37:12 +0530

    experiment:add required headers

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://frontend-server.onrender.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.listen(PORT);
