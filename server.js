6e8d40ad2c29eb0638284e24b80f408130919573
Yashwanth Reddy <yashwanth@renlans.org>
2022-08-02 23:38:57 +0530

    changes for interaction with local server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://frontend-server.onrender.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://localhost:8080"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
